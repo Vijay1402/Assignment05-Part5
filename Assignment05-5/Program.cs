d138419310485f4c12a59ce32ff0a63993b6a0ba
Vijayaraghavan R <rvrblr@gmail.com>
2023-11-09 14:49:37 +0530

    Assignment

---
+++ Assignment05-5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment05_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your exam score as percentage");
+            float per = float.Parse(Console.ReadLine());
+            if (per >= 90)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (per >= 80)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (per >= 70)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (per >= 60)
+            {
+                Console.WriteLine("Your grade is D");
+            }
+            else 
+            {
+                Console.WriteLine("Your grade is F");
+            }
+            Console.ReadKey();
+        }
+    }
+}
